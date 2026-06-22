52bb2ce9e09fc09ef1c6bf02d773a03c0d6d30ec
Ishtiak-Shikder <ishmamshikder01@gmail.com>
2026-06-22 18:10:20 +0600

    remove hashrouter

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createHashRouter,createHashRouter } from "react-router-dom";
 import App from "../../App";
 import Layouts from "../Layouts/Layouts";
 import Home from "../Pages/Home";
@@ -7,7 +7,7 @@ import DashBoard from "../Pages/DashBoard";
 
 import CoffeeCards from "../CoffeCards/CoffeeCards";
 
-const routes =createBrowserRouter([
+const routes = createHashRouter([
   {
     path: '/',
     element: <Layouts/>,
